8d67d9e30124f1aa3727c1f125bc6fec0cfafd2d
Shahboz <sharafzodashahboz@gmail.com>
2023-10-18 19:37:35 +0500

    admin login using email added

--- book/models.py
+++ book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from .validators import validate_decimal, validate_integer,validate_string 
-
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 
 
 # book model
@@ -49,6 +49,46 @@ class Rating(models.Model):
 
     def __str__(self):
         return str(self.book)
+    
+class UserProfileManager(BaseUserManager):
+    """ Manager for user profiles """
+
+    def create_user(self, email, name, password=None):
+        """ Create a new user profile """
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+    
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """ Database model for users in the system """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        """ Return string representation of our user """
+        return self.email
+    
+
 
 
 
